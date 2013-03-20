699f97f1e7d0c2eb75660138ea0dee00a5d4bea1
Stephen Paul Weber <singpolyma@singpolyma.net>
2013-03-20 17:09:05 -0500

    Select assertions out of DB in controller

--- Application.hs
+++ Application.hs
@@ -1,11 +1,15 @@
 module Application where
 
+import Data.String (fromString)
 import Network.Wai (Request(..), Response(..), Application)
 import Network.HTTP.Types (ok200, notFound404, seeOther303, badRequest400, notAcceptable406, Status, ResponseHeaders)
 import Network.Wai.Util (string, stringHeaders)
 import Web.PathPieces (PathPiece(..))
 import Data.Base58Address (RippleAddress)
 import Control.Error (readMay)
+import Control.Monad.Trans (liftIO)
+import Database.SQLite.Simple (query, field, FromRow(..), Connection, open, close)
+import Database.SQLite.Simple.ToField (ToField(..))
 import qualified Blaze.ByteString.Builder.Char.Utf8 as Blaze
 import qualified Data.Text as T
 import qualified Data.Text.Lazy.Builder as TL
@@ -18,6 +22,9 @@ import MustacheTemplates
 instance PathPiece RippleAddress where
 	fromPathPiece = readMay . T.unpack
 	toPathPiece = T.pack . show
+
+instance ToField RippleAddress where
+	toField adr = toField (show adr)
 
 htmlEscape :: String -> String
 htmlEscape = concatMap escChar
@@ -35,6 +42,8 @@ on404 :: Application
 on404 _ = string notFound404 [] "Not Found"
 
 reportFor :: Connection -> RippleAddress -> Application
-reportFor db adr req = return $ responseTextBuilder ok200 headers (viewReport htmlEscape $ Report adr)
+reportFor db adr req = do
+		assertions <- liftIO $ query db (fromString "SELECT `to`, `from`, `type`, `time`, `signed` FROM assertions WHERE `to` = ?") [adr]
+		return $ responseTextBuilder ok200 headers (viewReport htmlEscape $ Report adr assertions)
 	where
 	Just headers = stringHeaders [("Content-Type", "text/html; charset=utf8")]
--- Records.hs
+++ Records.hs
@@ -4,9 +4,12 @@ import Data.Base58Address (RippleAddress)
 import qualified Data.Text.Buildable as TL
 import qualified Data.Text.Format.Types as TL
 
+import Sqlite3
+
 instance TL.Buildable RippleAddress where
 	build adr = TL.build (TL.Shown adr)
 
 data Report = Report {
-		address :: RippleAddress
+		address :: RippleAddress,
+		assertions :: [AssertionRow]
 	}
